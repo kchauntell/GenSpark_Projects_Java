6dce9d39be68f0dd71ddf2502838a1ad9ed38eb7
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-29 11:07:40 -0500

    Try Catch added, everything works smoothly. Issue with if blank response or invalid response happens twice, it automatically shut out, but I like that feature. So i kept it and added a Warning for two Consecutive invalid/blank entries.

--- guess_the_number/src/Main.java
+++ guess_the_number/src/Main.java
@@ -1,95 +1,103 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
-    public static void main(String [] args) {
-        int count = 6;
-        Boolean play = true;
+class HelperClass {
+  public static int getRandomNumber () {
+    Random number = new Random();
+    int num = number.nextInt(1, 20);
+    return num;
+  }
 
-        while (play == true) {
-            Random num = new Random();
-            int random_number = num.nextInt(1, 20);
-            Scanner guess_the_number = new Scanner(System.in);
-            System.out.println("Welcome to the Guessing Game!");
-            System.out.println("Let's start by grabbing your name.");
-            System.out.println("What shall we call you?");
-            String username = guess_the_number.nextLine();
-            try {
-                switch (username) {
-                    case "" -> {
-                        throw new Exception();
-                    }
-                    default -> {
-                        System.out.println(String.format("Hello, %s, Let's Play!.", username));
-                        System.out.println("You get 6 lives! Let's see if you survive!");
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Please Provide a Player Name.");
-                continue;
-            }
-            System.out.println("What do you think the number is, between 1 and 20?");
-            while (count != 0) {
-                String guess = guess_the_number.nextLine();
-                try {
-                    Integer.parseInt((String) guess);
-                } catch (NumberFormatException e) {
-                    System.out.println("Caught Exception: Number Format Exception");
-                    System.out.println("Mistakes happen, you didn't lose a life, try again!");
-                    continue;
-                }
-                if (guess.equals(String.valueOf(random_number))) {
-                    System.out.println("You Guessed It!");
-                    System.out.println("Would you like to play again? (yes/no)");
-                    String playAgain = guess_the_number.nextLine();
-                        if (playAgain.equals("no")) {
-                            System.out.println("GoodBye");
-                            play = false;
-                            break;
-                        } else if (playAgain.equals("yes")){
-                            main(args);
-                        }
-                    break;
-                } else if (!guess.equals(String.valueOf(random_number)) && count != 0) {
-                    count--;
-                    if (count == 0) {
-                        System.out.println(String.format("You are out of lives, %s!", username));
-                        System.out.println("Better luck next time!");
-                        System.out.println("Would you like to play again? (yes/no)");
-                        String playAgain = guess_the_number.nextLine();
-                        try {
-                            if (playAgain.equals("no")) {
-                                System.out.println("GoodBye");
-                                play = false;
-                                break;
-                            } else if (playAgain.equals("yes")){
-                                main(args);
-                            } else {
-                                throw new Exception();
-                            }
-                        } catch (Exception e) {
-                            System.out.println("Please respond with yes or no.");
-                        }
-                        break;
-                    }
-                    int inputGuess = Integer.parseInt((String) guess);
-                    try {
-                        if(inputGuess > 20 || inputGuess <= 0) throw new Exception();
-                        if (inputGuess > random_number) {
-                            System.out.println("Guess is too High!");
-                            System.out.println(String.format("You have %s lives left. Try again!", count));
-                        } else if (inputGuess < random_number) {
-                            System.out.println("Guess is too Low! Give it one more try!");
-                            System.out.println(String.format("You have %s lives left. Try again!", count));
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Invalid Entry: You picked a number out of range.");
-                        System.out.println("You STILL lose a life! Pay Attention, between 1-20!");
-                        System.out.println(String.format("You have %s lives left. Try again!", count));
-                    }
-                }
-
-            }
-        }
+  public static String playAgain(String answer) {
+    Boolean result = null;
+    try {
+      switch (answer) {
+        case "yes" -> result = true;
+        case "no" -> result = false;
+        case "" -> throw new Exception();
+      }
+    } catch (Exception e) {
+      System.out.println("Blank Response/Invalid Response: Need to answer yes or no.");
+      System.out.println("Two consecutive Invalid/Blank Responses is automatic shut down of Game!");
     }
+    return String.valueOf(result);
+  }
+
+  public static String Guesses(int one, int two) {
+    String result = "";
+    if(one > two)  result = "Too High!";
+    else if (one < two) result = "Too Low!";
+    else if (one == two) result = "Correct!";
+    return result;
+  }
+}
+
+public class Main {
+  public static void main(String [] args) {
+    int random_num = HelperClass.getRandomNumber();
+    int count = 6;
+    System.out.println(random_num);
+    Scanner guess_the_number = new Scanner(System.in);
+    System.out.println("Welcome to the Guessing Game!");
+    System.out.println("Let's start by grabbing your name.");
+    System.out.println("What shall we call you?");
+    String username = guess_the_number.nextLine();
+    try {
+      if (username.equals("")) throw new Exception();
+      else System.out.println(String.format("Hello, %s, Let's Play!.", username));
+    } catch (Exception e) {
+      System.out.println("Please Provide a name.");
+      username = guess_the_number.nextLine();
+      System.out.println(String.format("Hello, %s, Let's Play!.", username));
+    }
+    System.out.println("What do you think the number is, between 1 and 20?");
+    while(count !=0) {
+      String guess = guess_the_number.nextLine();
+      try {
+        int numGuess = Integer.parseInt(guess);
+        switch(HelperClass.Guesses(numGuess, random_num)) {
+          case "Too High!" -> {
+            count--;
+            System.out.println("Guessed Too High! Try Again.");
+            System.out.println(String.format("You have %s lives left.", count));
+          }
+          case "Too Low!" -> {
+            count--;
+            System.out.println("Guess Too Low! Try Again.");
+            System.out.println(String.format("You have %s lives left.", count));
+          }
+          case "Correct!" -> {
+            System.out.println("You Guessed it!");
+            count = 0;
+          }
+          default -> throw new Exception();
+        }
+      } catch (Exception e){
+        System.out.println("That was not a valid guess.");
+//        guess = guess_the_number.nextLine();
+      }
+    }
+    System.out.println("Would you like to play again? (yes/no)");
+    String answer = guess_the_number.nextLine();
+    try {
+      switch(HelperClass.playAgain(answer)) {
+        case "true" -> main(args);
+        case "false" -> {
+          System.out.println("GoodBye");
+          break;
+        }
+        default -> throw new Exception();
+      }
+    } catch (Exception e) {
+      String newAnswer = guess_the_number.nextLine();
+      switch(HelperClass.playAgain(newAnswer)) {
+        case "true" -> main(args);
+        case "false" -> {
+          System.out.println("GoodBye");
+          break;
+        }
+        default -> {break;}
+      }
+    }
+  }
 }
