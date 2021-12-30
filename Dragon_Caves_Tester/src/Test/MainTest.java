2deb6d004068bc521a48bcb77888d9acbd72b072
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-30 14:06:38 -0500

    Added some descriptors.

--- Dragon_Caves_Tester/src/Test/MainTest.java
+++ Dragon_Caves_Tester/src/Test/MainTest.java
@@ -1,5 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MainTest {
@@ -7,19 +8,21 @@ public class MainTest {
   Main dragonTest;
 
 
+  //this will run before each test, multiple test = multiple before actions
   @BeforeEach
   void setUp() {
-
+    //creates a new object.
     dragonTest = new Main();
   }
 
   @DisplayName("Test Chosen Path")
   @Test
+  //will test whichPath method with each potential option.
   void whichPath() {
-    assertEquals("wealth", dragonTest.whichPath("1"), "Should return 'wealth'");
-    assertEquals("death", dragonTest.whichPath("2"), "Should return 'death'");
-    assertEquals("blank", dragonTest.whichPath(""), "Should return 'blank'");
-    assertEquals("noOption", dragonTest.whichPath("7"), "Should return 'noOption'");
+    Assertions.assertEquals("wealth", dragonTest.whichPath("1"), "Should return 'wealth'");
+    Assertions.assertEquals("death", dragonTest.whichPath("2"), "Should return 'death'");
+    Assertions.assertEquals("blank", dragonTest.whichPath(""), "Should return 'blank'");
+    Assertions.assertEquals("noOption", dragonTest.whichPath("7"), "Should return 'noOption'");
   }
 
   @AfterEach
