3d3f4d6ad9b43c7726971fa8a630393575ca4722
Kendra Chauntell <kendrachauntell@outlook.com>
2022-01-09 15:54:08 -0500

    Initial Creation HangMan
    Initial creation and push of Hangman Project.

---
+++ hangman_Project/src/Java/hangMan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class hangMan {
+
+  //Methods to use in Main class for HangMan
+  public static int getRandomNumber() {
+    Random number = new Random();
+    return number.nextInt(1, 5);
+  };
+
+
+
+
+  public static void main(String[] args) {
+
+
+  }
+}
