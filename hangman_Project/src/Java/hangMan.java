fcfccefd7ea163378cf4551f71fcb38f79cc6845
Kendra Chauntell <kendrachauntell@outlook.com>
2022-01-09 16:38:48 -0500

    Added Two Methods
    Random Number Method and Random work grabbed Method.

--- hangman_Project/src/Java/hangMan.java
+++ hangman_Project/src/Java/hangMan.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class hangMan {
 
   //Methods to use in Main class for HangMan
   public static int getRandomNumber() {
     Random number = new Random();
-    return number.nextInt(1, 5);
+    return number.nextInt(0, 2);
   };
 
+  public static String getRandomWord(int number) {
+    List<String> wordList = Arrays.asList("basketball", "java", "awesome");
+    ArrayList<String> wordArrList  = new ArrayList<String>();
+    wordArrList.addAll(wordList);
 
+    return wordArrList.get(number);
+  }
 
 
   public static void main(String[] args) {
+    int randomSelection = getRandomNumber();
+    String randomWordToGuess = getRandomWord(randomSelection);
 
 
   }
