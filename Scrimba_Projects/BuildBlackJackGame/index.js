609cd2e665d5d42549d973b83048917eaa59984b
Kendra Chauntell <kendrachauntell@outlook.com>
2022-02-26 00:34:16 -0500

    Finished BlackJack Project Lesson for Scrimba
    basic build with BlackJack. will need to go back to add more features of the basic game.

--- Scrimba_Projects/BuildBlackJackGame/index.js
+++ Scrimba_Projects/BuildBlackJackGame/index.js
@@ -1,25 +1,44 @@
-
 function getRandomArbitrary(min,max) {
-  //max exclusive for this function.
-  return Math.floor(Math.random() * (max - min) + min);
+  let random = Math.floor(Math.random() * (max - min) + min);
+  if (random === 1) return 11;
+  else if(random > 10) return 10;
+  else return random;
 }
 
-let firstCard = getRandomArbitrary(2,12);
-let secondCard = getRandomArbitrary(2,12);
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let msg;
 
 let msgEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
 let cardsEl = document.getElementById('cards-el');
 
-const startGame = () => renderGame();
+let player = {
+  name: "Kendra",
+  chips: 100,
+}
+
+let playerEl = document.getElementById('player-el');
+playerEl.textContent = `${player.name}: \$${player.chips}`;
+
+const startGame = () => {
+  isAlive = true;
+  let firstCard = getRandomArbitrary(1, 14);
+  let secondCard = getRandomArbitrary(1, 14);
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
 
 const renderGame = () => {
-  cardsEl.textContent = `Cards: ${firstCard}  ${secondCard}`;
+  cardsEl.textContent = "Cards: "
+
+  for(let card of cards) {
+    cardsEl.textContent += card + "  ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
 
   if (sum <= 20) {
@@ -37,7 +56,11 @@ const renderGame = () => {
 }
 
 let newCard =() => {
-  let card = getRandomArbitrary(2,12);
-  sum += card;
-  startGame();
+  if(isAlive && !hasBlackJack) {
+    let card = getRandomArbitrary(1,14);
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
 }
